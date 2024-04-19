69664b5ae7b5eff6da16936aaab599b08fc00f81
Patrik-Bjorkman <patrikbman94@gmail.com>
2024-04-19 11:19:28 +0200

    task 3

--- static/js/Tasks/task3.js
+++ static/js/Tasks/task3.js
@@ -1,19 +1,66 @@
 mymap = window.mymap;
 
-var supermarketLayer = L.geoJson(supermarket);
-
-supermarketLayer = L.geoJSON(supermarket, {
-  onEachFeature: function (feature, layer) {
-    layer.bindPopup(feature.properties.name);
-  },
+var supermarketLayer = L.geoJSON(supermarket, {
+	onEachFeature: function (feature, layer) {
+		layer.bindPopup(feature.properties.name);
+	},
 });
 
-btnTask3 = document.getElementById("btnTask3");
+function createBufferedZones(geojsonData, radius) {
+	var bufferedFeatures = geojsonData.features.map((feature) => {
+		return turf.buffer(feature, radius, { units: 'kilometers' });
+	});
+	return turf.featureCollection(bufferedFeatures);
+}
 
-btnTask3.addEventListener("click", function () {
-  if (mymap.hasLayer(supermarketLayer)) {
-    mymap.removeLayer(supermarketLayer);
-  } else {
-    supermarketLayer.addTo(mymap);
-  }
+var bufferLayer = L.geoJSON(createBufferedZones(supermarket, 1), {
+	style: function () {
+		return { color: 'grey', weight: 1, opacity: 0.2 };
+	},
+});
+
+function checkIsolation(geojsonData, bufferRadius) {
+	var buffers = geojsonData.features.map((feature) =>
+		turf.buffer(feature, bufferRadius, { units: 'kilometers' })
+	);
+	var isolatedFeatures = [];
+
+	for (let i = 0; i < buffers.length; i++) {
+		let isIsolated = true;
+		for (let j = 0; j < buffers.length; j++) {
+			if (i !== j && turf.intersect(buffers[i], buffers[j])) {
+				isIsolated = false;
+				break;
+			}
+		}
+		if (isIsolated) {
+			isolatedFeatures.push(geojsonData.features[i]);
+		}
+	}
+
+	return turf.featureCollection(isolatedFeatures);
+}
+
+function highlightIsolatedSupermarkets() {
+	var isolatedSupermarkets = checkIsolation(supermarket, 1);
+	var isolatedBuffers = createBufferedZones(isolatedSupermarkets, 1);
+
+	L.geoJSON(isolatedBuffers, {
+		style: function () {
+			return { color: 'red', weight: 2, opacity: 0.8 };
+		},
+	}).addTo(mymap);
+}
+
+btnTask3 = document.getElementById('btnTask3');
+btnTask3.addEventListener('click', function () {
+	if (mymap.hasLayer(supermarketLayer) || mymap.hasLayer(bufferLayer)) {
+		mymap.removeLayer(supermarketLayer);
+		mymap.removeLayer(bufferLayer);
+	} else {
+		mymap.setView([59.99127905073191, 17.820411761712652], 9);
+		mymap.addLayer(supermarketLayer);
+		mymap.addLayer(bufferLayer);
+		highlightIsolatedSupermarkets();
+	}
 });
